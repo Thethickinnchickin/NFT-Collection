0295a9741ec281cda69b1ca69a9d09adc47b6d60
Matt <matt.reiley@sjsu.edu>
2023-01-27 20:26:03 -0800

    initial commit

---
+++ constants/index.js
@@ -0,0 +1,5 @@
+const WHITELIST_CONTRACT_ADDRESS = "0xc48e951460D233129f44809d9c2d3315a09AB0BE";
+
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL};
